8ac4c8540e052ed173f3d9c601f9c618bd81c60b
 Asaad Baz <asaadmbaz@MacBook-Pro-Asaad.local>
2025-08-03 22:44:40 +0300

    #2Problem -Perfect Number

---
+++ #2Problem-C5.cpp
@@ -0,0 +1,63 @@
+// Problem #2 Perfect Number
+
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int ReadPositiveNumber (string Message)
+
+{
+    int Number= 0; // هون مشان ما ياخد قيمة عشوائية من الذاكرة
+
+    do {
+        cout<<Message<<endl;
+        cin>> Number ;
+    } while (Number <= 0);
+
+    return Number; 
+    
+}
+
+bool isNotPerfect (int Number)
+{
+    int Sum = 0;
+
+    for (int i = 1 ; i < Number ; i ++ )
+    {
+        if (Number % i == 0  )
+        Sum += i;
+        
+    }
+
+    return Sum == Number ;
+}
+
+void PrintResults ( int Number)
+{
+    if (isNotPerfect(Number))
+    cout<<"The Number is Perfect \n";
+    else 
+    cout << "The Number is NOT perfect \n";
+}
+
+
+void PrintFrom1ToN (int Number)
+
+{
+    for (int i = 1 ; i < Number ; i++)
+        {
+            if ( isNotPerfect(i)) {
+
+                cout<<i<<"\n"; 
+            }
+
+        }
+}
+
+int main ()
+{
+    PrintFrom1ToN(ReadPositiveNumber("Please Enter a Positive Number"));
+
+    return 0;
+}
