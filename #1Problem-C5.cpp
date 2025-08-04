909f814abbb1befab7741a76383c0f4efb2a4b23
 Asaad Baz <asaadmbaz@MacBook-Pro-Asaad.local>
2025-08-04 12:40:57 +0300

    Mutliplaction Table- #1Problem

---
+++ #1Problem-C5.cpp
@@ -0,0 +1,54 @@
+// Problem #1 Multiplication Table from 1 to 10
+
+
+#include <iostream>
+#include <math.h>
+#include <string>
+using namespace std;
+
+void PrintTableHeader ()
+{
+    cout << "\n\n\t\t\t Multiplication Table from 1 - 10\n\n"; 
+    cout <<"\t ";
+
+    for (int i = 1 ; i <= 10; i++)
+    {
+        cout << i <<"\t "; 
+    }
+
+    cout<< "\n______________________________________________________________________________\n";
+}
+
+string ColumSperator(int i) 
+{
+    if (i < 10)
+    return "  |"; 
+    else
+
+    return " |";
+    }
+
+
+void PrintMultiplicationTable()
+{
+    PrintTableHeader();
+
+for (int i=1 ; i<= 10; i++)
+{
+    cout<<" "<< i << ColumSperator(i)<<"\t";
+
+    for (int j=1; j<= 10; j++)
+    {
+        cout << i * j <<"\t";
+    }
+
+    cout<<endl;
+}
+
+}
+
+int main ()
+{
+
+    PrintMultiplicationTable();
+}
